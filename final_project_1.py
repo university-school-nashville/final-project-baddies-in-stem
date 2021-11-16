6a8fe46b4ba52422f82333efa4e79d2a60792ca6
abigailgreen23 <abigailgreen23@usn.org>
2021-11-16 09:12:08 -0600

    Update final_project_1.py

--- final_project_1.py
+++ final_project_1.py
@@ -16,15 +16,22 @@ FPS = 60
 class Player:
     def __init__(self):
         self._location = "attic"
+        
+def goto(room):
+    display.blit(room,(0,0))
 
 atticBackground = pygame.image.load("final_project_attic.jpeg")
 atticBackground = pygame.transform.scale(atticBackground,(750,500))
 
 def Escape(player):
     
+    player = Player()
+    
+    goto(atticBackground)
+    pygame.display.update()
+    
     while player._location == "attic":
-        display.blit(atticBackground,(0,0))
-        pygame.display.update()
+        
         
         ev = pygame.event.get()
         for event in ev:
@@ -33,5 +40,6 @@ def Escape(player):
         
     
     pygame.QUIT
-    
-Escape()
+  
+x = "player"
+Escape(x)
